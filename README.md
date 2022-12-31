5f2393de8a6463ec609d6eea8ef023005ea536a4
evyatar9 <evyatar98@gmail.com>
2022-12-31 18:08:41 +0200

    ExplainThisCode

---
+++ README.md
@@ -0,0 +1,81 @@
+# ETC (Explain This Code)
+
+[**E**xplain**T**his**C**ode](https://marketplace.visualstudio.com/items?itemName=evyatar9.etc) is A VSCode extension that uses the ChatGPT API to provide explanations for selected code.
+
+![explain-this-code.gif](https://github.com/evyatar9/ExplainThisCode/raw/master/images/explain-this-code.gif?raw=true)
+
+
+## Features
+
+- The extension will explain the selected code in your VSCode editor and insert the explanation as a block comment above the code.
+
+- Run this extension using one of the following methods:
+
+  - Keyboard shortcut:
+    1. **Windows and Linux:** Ctrl + Alt + E
+    2. **macOS:** Ctrl + Cmd + E
+
+  - `ETC` command in the command palette.
+
+## Requirements
+
+- [A ChatGPT API key](https://beta.openai.com/account/api-keys) is required to use this extension.
+
+## Installation
+
+There are two ways to install this extension:
+
+- **From the Extension Marketplace**: To install this extension from the Extension Marketplace in Visual Studio Code, search for [**E**xplain**T**his**C**ode](https://marketplace.visualstudio.com/items?itemName=evyatar9.etc) and click the Install button.
+
+- **Manually using a .vsix file**: To install this extension manually using a .vsix file, follow these steps:
+  1. Download the .vsix file from the releases page on GitHub [Releases](https://github.com/evyatar9/ExplainThisCode/releases)
+
+  2. In Visual Studio Code, open the Command Palette (Ctrl+Shift+P)
+
+  3. Select "Extensions: Install from VSIX..." and select the .vsix file you just downloaded.
+
+## Extension Settings
+
+- `explainThisCode.apiKey`: ChatGPT API key to use with the extension.
+
+To set the value of the `explainThisCode.apiKey` extension setting:
+
+1. Open the settings editor by selecting the `File` menu, then `Preferences` and then `Settings`, or by using the keyboard shortcut `Ctrl + ,` (Windows) or `Command + ,` (Mac).
+
+2. In the search bar at the top of the editor, search for `explainThisCode.apiKey`.
+
+3. Click on the `explainThisCode.apiKey` setting and enter your ChatGPT API key in the input field provided.
+
+4. Click the `Save` button to save your changes.
+
+Alternatively, you can also set the value of the `explainThisCode.apiKey` setting using the `set()` function of the `ConfigurationChangeEvent` class. For example:
+
+```javascript
+vscode.workspace.getConfiguration().update('explainThisCode.apiKey', 'my-api-key', vscode.ConfigurationTarget.Global);
+```
+
+This will set the value of the `explainThisCode.apiKey` setting to `'my-api-key'` for all workspaces.
+
+## Release Notes
+
+### 1.0.0
+
+- Initial release of the ETC extension.
+
+## Credits
+
+- ChatGPT API: [https://openai.com/docs/api-reference/overview/](https://openai.com/docs/api-reference/overview/)
+
+## License
+
+This extension is licensed under the [MIT License](https://chat.openai.com/LICENSE).
+
+## Contact
+
+Telegram: [@evyatar9](https://t.me/evyatar9)
+
+Discord: [evyatar9#5800](https://discordapp.com/users/812805349815091251)
+
+## Contributing
+
+If you would like to contribute to the ETC extension, feel free to submit a pull request or report any issues you encounter on the [**E**xplain**T**his**C**ode repository](https://github.com/evyatar9/ExplainThisCode).
---
+++ extension.js
@@ -0,0 +1,166 @@
+const vscode = require('vscode');
+const path = require('path');
+const https = require('https');
+
+
+function setLanguageConfigMap() {
+	let languageConfigFiles = new Map();
+
+	// Set language configuration path on Map
+	vscode.extensions.all.forEach(extension => {
+		let packageJSON = extension.packageJSON;
+
+		if (packageJSON.contributes && packageJSON.contributes.languages) {
+			packageJSON.contributes.languages.forEach(language => {
+				if (typeof language.configuration === 'string' && typeof language.id === 'string') {
+					let configPath = path.join(extension.extensionPath, language.configuration);
+					if (!languageConfigFiles.has(language.id)) {
+						languageConfigFiles.set(language.id, configPath);
+					}
+				}
+			});
+		}
+	});
+
+	return languageConfigFiles;
+}
+
+
+async function GetCommentConfiguration(languageConfigFiles, commentConfig, languageCode) {
+	// Return undefined if the language is not supported
+	if (!languageConfigFiles.has(languageCode)) {
+		return undefined;
+	}
+
+	// Return the language configuration if it is already in the map
+	if (commentConfig.has(languageCode)) {
+		return commentConfig.get(languageCode);
+	}
+
+	try {
+		// Get the language configuration file path
+		const filePath = languageConfigFiles.get(languageCode);
+		const rawContent = await vscode.workspace.fs.readFile(vscode.Uri.file(filePath));
+		const content = new TextDecoder().decode(rawContent);
+		const config = JSON.parse(content);
+
+		// Set the default value for the comments field
+		config.comments = config.comments || {};
+
+		// Set the language configuration in the map
+		commentConfig.set(languageCode, config.comments);
+
+		return config.comments;
+	} catch (error) {
+		commentConfig.set(languageCode, undefined);
+		return undefined;
+	}
+}
+
+async function explainCode(code, api_key) {
+
+	const MODEL = 'text-davinci-003';
+	const prompt = "Explain this code:\n" + code;
+	const options = {
+		hostname: 'api.openai.com',
+		port: 443,
+		path: '/v1/completions',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Authorization': `Bearer ${api_key}`
+		}
+	};
+	let responseData = '';
+	let statusCode = 0;
+
+	const req = https.request(options, res => {
+		statusCode = res.statusCode;
+
+		res.on('data', d => {
+			responseData += d;
+		});
+	});
+
+	req.on('error', error => {
+		console.error(error);
+	});
+
+	req.write(JSON.stringify({
+		model: MODEL,
+		prompt: prompt,
+		max_tokens: 2048
+	}));
+	req.end();
+
+	await new Promise(resolve => req.on('close', resolve));
+
+	if (statusCode == 401) {
+		throw new Error('The API key provided is not valid. Please check the key and try again.');
+	}
+
+	return JSON.parse(responseData);
+
+}
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+async function activate(context) {
+
+	let commentConfig = new Map();
+	let api_key = vscode.workspace.getConfiguration().get('explainThisCode.apiKey');
+	let languageConfigFiles = setLanguageConfigMap();
+	
+
+	let disposable = vscode.commands.registerCommand('explain-this-code.explainCode', async function () {
+		try {
+			const editor = vscode.window.activeTextEditor;
+			if (!api_key) {
+				throw new Error('API key is required and cannot be empty');
+			}
+
+			if (!editor) {
+				throw new Error('An unexpected error has occurred.');
+				return;
+			}
+
+			const selectedText = editor.document.getText(editor.selection);
+			if (!selectedText) {
+				vscode.window.showErrorMessage('An explanation can only be provided for selected code. Please select the code you want to have explained.');
+				return;
+			}
+			vscode.window.showInformationMessage('An explanation of the selected code will be shown in the form of comments above it.');
+			const explain = await explainCode(selectedText, api_key);
+			if (explain['error']) {
+				vscode.window.showErrorMessage(explain['error']['message']);
+				return;
+			}
+
+			const comments = commentConfig.get(editor.document.languageId) || await GetCommentConfiguration(languageConfigFiles, commentConfig, editor.document.languageId);
+			const blockCommentStart = comments.blockComment ? comments.blockComment[0] : null;
+			const blockCommentEnd = comments.blockComment ? comments.blockComment[1] : null;
+
+			const editRange = editor.document.lineAt(editor.selection.start.line).range.start;
+
+			editor.edit(editBuilder => {
+				if (editor !== undefined) {
+					editBuilder.insert(editRange, blockCommentStart + explain["choices"][0]["text"] + "\n" + blockCommentEnd + "\n");
+				}
+			});
+		} catch (error) {
+			vscode.window.showErrorMessage('An error occurred while activating the extension: ' + error.message);
+			disposable.dispose();
+			disposable = undefined;
+			activate(context);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+function deactivate() { }
+
+module.exports = {
+	activate,
+	deactivate
+}
